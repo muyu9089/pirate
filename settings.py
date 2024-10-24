d05876010553927ebc1fda0c80e7bde384c03e8f
muyu9089 <908929260@qq.com>
2024-10-25 00:13:33 +0800

    Update settings.py and spider.py. Optimize code structure

--- settings.py
+++ settings.py
@@ -1,46 +1,52 @@
 import requests
  
-cookies = {
-    'qgqp_b_id': '02d480cce140d4a420a0df6b307a945c',
-    'cowCookie': 'true',
-    'em_hq_fls': 'js',
-    'intellpositionL': '1168.61px',
-    'HAList': 'a-sz-300059-%u4E1C%u65B9%u8D22%u5BCC%2Ca-sz-000001-%u5E73%u5B89%u94F6%u884C',
-    'st_si': '07441051579204',
-    'st_asi': 'delete',
-    'st_pvi': '34234318767565',
-    'st_sp': '2021-09-28%2010%3A43%3A13',
-    'st_inirUrl': 'http%3A%2F%2Fdata.eastmoney.com%2F',
-    'st_sn': '31',
-    'st_psi': '20211020210419860-113300300813-5631892871',
-    'intellpositionT': '1007.88px',
-}
- 
-headers = {
-    'Connection': 'keep-alive',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50',
-    'DNT': '1',
-    'Accept': '*/*',
-    'Referer': 'http://quote.eastmoney.com/',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-}
- 
-params = (
-    ('cb', 'jQuery112404825022376475756_1634735261901'),
-    ('pn', '1'),
-    ('pz', '20'),
-    ('po', '1'),
-    ('np', '1'),
-    ('ut', 'bd1d9ddb04089700cf9c27f6f7426281'),
-    ('fltt', '2'),
-    ('invt', '2'),
-    ('fid', 'f3'),
-    ('fs', 'm:0 t:6,m:0 t:80,m:1 t:2,m:1 t:23'),
-    ('fields', 'f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152'),
-    ('_', '1634735261902'),
-)
+class Settings:
+    
+    def __init__(self, page):
+        self.page = page
+        
+        self.url = 'http://54.push2.eastmoney.com/api/qt/clist/get'
+        
+        self.cookies = {
+            'qgqp_b_id': '02d480cce140d4a420a0df6b307a945c',
+            'cowCookie': 'true',
+            'em_hq_fls': 'js',
+            'intellpositionL': '1168.61px',
+            'HAList': 'a-sz-300059-%u4E1C%u65B9%u8D22%u5BCC%2Ca-sz-000001-%u5E73%u5B89%u94F6%u884C',
+            'st_si': '07441051579204',
+            'st_asi': 'delete',
+            'st_pvi': '34234318767565',
+            'st_sp': '2021-09-28%2010%3A43%3A13',
+            'st_inirUrl': 'http%3A%2F%2Fdata.eastmoney.com%2F',
+            'st_sn': '31',
+            'st_psi': '20211020210419860-113300300813-5631892871',
+            'intellpositionT': '1007.88px',
+        }  
+        
+        self.headers = {
+            'Connection': 'keep-alive',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50',
+            'DNT': '1',
+            'Accept': '*/*',
+            'Referer': 'http://quote.eastmoney.com/',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+        }
+        
+        self.params = (
+            ('cb', 'jQuery112404825022376475756_1634735261901'),
+            ('pn', str(self.page)),
+            ('pz', '20'),
+            ('po', '1'),
+            ('np', '1'),
+            ('ut', 'bd1d9ddb04089700cf9c27f6f7426281'),
+            ('fltt', '2'),
+            ('invt', '2'),
+            ('fid', 'f3'),
+            ('fs', 'm:0 t:6,m:0 t:80,m:1 t:2,m:1 t:23'),
+            ('fields', 'f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152'),
+            ('_', '1634735261902'),
+        )
 
- 
 #NB. Original query string below. It seems impossible to parse and
 #reproduce query strings 100% accurately so the one below is given
 #in case the reproduced version is not "correct".
--- spider.py
+++ spider.py
@@ -1,24 +1,48 @@
 '''
+Descripttion: 
+Author: yang fu ren
+version: 
+Date: 2024-10-23 22:55:39
+LastEditors: yang fu ren
+LastEditTime: 2024-10-25 00:05:35
+'''
+'''
 Descripttion: 东方财富信息爬虫主函数
 Author: 
 version: v1
 Date: 2024-10-23 22:55:39
 LastEditors: yang fu ren
-LastEditTime: 2024-10-24 01:05:14
+LastEditTime: 2024-10-24 23:40:34
 '''
 import requests 
 import re
 import pandas as pd
 
-from settings import *
+from settings import Settings
 
-df = pd.DataFrame(columns=list(['代码', '名称', '最新价', '涨跌幅', '涨跌额', '成交量']))
- 
-response = requests.get('http://54.push2.eastmoney.com/api/qt/clist/get', headers=headers, params=params, cookies=cookies, verify=False)
-
-if response.status_code == 200:
-    try:
-        # 正则获取相关信息
+class Spider:
+    
+    def __init__(self, url, headers, params, cookies, verify, stock_df):
+         self.url = url
+         self.headers = headers
+         self.params = params
+         self.cookies = cookies
+         self.verify = verify
+         self.stock_df = stock_df
+         
+    def get_respones_from_url(self):
+        try:
+            response = requests.get(url=self.url, headers=self.headers, params=self.params, cookies=self.cookies, verify=self.verify)
+        
+            return response
+        except requests.exceptions.RequestException as e:
+            print(f"请求错误，错误信息：{e}")
+    
+    def get_stock_info_from_respones(self):
+        
+        response = self.get_respones_from_url()
+        
+         # 正则获取相关信息
         daimas = re.findall('"f12":(.*?),',response.text)
         names = re.findall('"f14":"(.*?)"',response.text)
         zuixinjias = re.findall('"f2":(.*?),',response.text)
@@ -35,13 +59,24 @@ if response.status_code == 200:
         huanshoulvs = re.findall('"f8":(.*?),',response.text)       
         shiyinglvs = re.findall('"f9":(.*?),',response.text)
         
+        if response.status_code == 200:
+                for i in range(20):
+                        list = [daimas[i], names[i], zuixinjias[i], zhangdiefus[i], zhangdiees[i], chengjiaoliangs[i]]
+                        self.stock_df.loc[len(self.stock_df)] = list
+        else:
+            print("请求失败， 状态码：{response.status_code}")
+
+
+if __name__ == "__main__":
+    
+    columns = ['代码', '名称', '最新价', '涨跌幅', '涨跌额', '成交量']
+    stock_df = pd.DataFrame(columns=columns)
+    
+    for page in range(1, 20):
         
-        for i in range(20):
-                list = [daimas[i], names[i], zuixinjias[i], zhangdiefus[i], zhangdiees[i], chengjiaoliangs[i]]
-                df.loc[len(df)] = list
-                
-        print(df)
-    except:
-        print("响应内容错误")
-else:
-    print("请求失败， 状态码：{response.status_code}")
+        settings = Settings(page)
+    
+        spider = Spider(settings.url, settings.headers, settings.params, settings.cookies, False, stock_df)
+        spider.get_stock_info_from_respones()
+
+    print(f"stock_df: {stock_df}")
